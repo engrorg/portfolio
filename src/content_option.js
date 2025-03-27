99d1f043ec4cab3b87041db5afd36a3cfcac6a9b
engrorg <h.hasan25@hotmail.com>
2025-03-28 01:31:57 +0600

    This is a new commit for what I originally planned to be amended

---
+++ src/content_option.js
@@ -0,0 +1,189 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+const logotext = "Hashibul.me";
+const meta = {
+    title: "Full-Stack Freelance Developer-Hashibul Hasan",
+    description: "I’m Hashibul Hasan _ Full stack devloper,currently working as a freelancer.",
+};
+
+const introdata = {
+    title: "I’m Hashibul Hasan",
+    animated: {
+        first: "Full-Stack Developer",
+        second: "MERN Developer",
+        third: "Freelancer",
+    },
+    description: "I'm a Software Engineer. More than five years of developing experience after graduating from Computer Science and Engineering in the year 2019.",
+    your_img_url: "https://avatars.githubusercontent.com/u/142114736?v=4",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I'm a tech enthusiast, as a computer science graduate. I've been working with different technology and programming language since 2011. I've developed different software and web application using nodejs,expressjs, laravel and postgresql. ",
+};
+const worktimeline = [
+    {
+        jobtitle: "Freelance Developer",
+        where: "Upwork",
+        date: "2025",
+    },
+    {
+        jobtitle: "MERN Developer",
+        where: "UpWork",
+        date: "2024",
+    },
+    {
+        jobtitle: "Full-Stack Developer",
+        where: "Gazipur",
+        date: "2020",
+    },
+    {
+        jobtitle: "Ecommerce Development",
+        where: "Gazipur",
+        date: "2019",
+    },
+    {
+        jobtitle: "React App Design",
+        where: "Dhaka",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "NodeJS",
+        value: 90,
+    },
+    {
+        name: "Express",
+        value: 92,
+    },
+    {
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "PostgreSQL",
+        value: 92,
+    },
+    {
+        name: "RESTAPI",
+        value: 95,
+    },
+];
+
+const services = [{
+        title: "Full-Stack Development- NodeJs, ExpressJs, RestApi and PostgreSQL",
+        description: "I'm a proficient Full Stack Developer specializing in Node.js, Express.js, RESTful APIs, and PostgreSQL. I build scalable backend systems, ensuring efficient data handling and seamless API integrations. With expertise in Express.js, I develop robust server-side applications, while their deep understanding of PostgreSQL ensures optimized database performance.",
+    },
+    {
+        title: "Ecommerce Development",
+        description: "I'm a skilled E-commerce Developer specializing in Node.js, Express.js, RESTful APIs, and PostgreSQL. I build secure, scalable, and high-performance e-commerce platforms, ensuring seamless shopping experiences. With expertise in Express.js, they develop robust backend systems, while their deep knowledge of PostgreSQL ensures efficient database management for products, orders, and users.                                                                                                  I will excel in API development, payment gateway integration, authentication, and performance optimization, creating seamless connections between the frontend and backend. Passionate about delivering fast, secure, and user-friendly e-commerce solutions, I stay updated with the latest technologies to enhance online shopping experiences.",
+    },
+    {
+        title: "Front-End Design - React",
+        description: "I'm a creative Front-End Designer specializing in React.js. With a keen eye for design and a passion for user experience, they craft visually stunning and highly interactive web applications. Their expertise in React, HTML, CSS, and JavaScript allows them to build responsive and dynamic interfaces that enhance usability.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "h.hasan25@hotmail.com",
+    YOUR_FONE: "+8801787492679",
+    description: "To connect with me just leave a message in whatsapp above number or send me a text here. We will discuss in more details about your project so that we can start it right way. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_f19ll9s",
+    YOUR_TEMPLATE_ID: "template_g5ct9vm",
+    YOUR_USER_ID: "EVETgmQE-WzGyY4ox",
+};
+
+const socialprofils = {
+    whatsapp:"https://wa.me/+8801787492679?text=Hello!%20I%20want%20to%20chat%20with%20you",
+    github: "https://github.com/engrorg",
+    // facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/hasan365",
+    twitter: "https://twitter.com",
+    stackoveflow:"https://stackoverflow.com/users/13791606/h-hasan"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};

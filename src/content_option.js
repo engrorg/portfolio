77b14efbba589d4da2da71584847b9a27e044040
engrorg <h.hasan25@hotmail.com>
2025-03-28 22:12:37 +0600

    This is a new commit add homepage_image_updated re

--- src/content_option.js
+++ src/content_option.js
@@ -8,14 +8,14 @@ const meta = {
 };
 
 const introdata = {
-    title: "I’m Hashibul Hasan",
+    title: "Full-Stack Freelance Developer -  Hashibul Hasan",
     animated: {
         first: "Full-Stack Developer",
-        second: "MERN Developer",
+        second: "MERN Stack",
         third: "Freelancer",
     },
-    description: "I'm a Software Engineer with More than five years of developing experience after graduating from Computer Science and Engineering in the year 2019.",
-    your_img_url: "https://avatars.githubusercontent.com/u/142114736?s=400&u=628688cc752c1f0c68a8897d279e3887320a6d87&v=4",
+    description: "I'm a software engineer with more than five years of development experience after graduating from computer science and engineering in the year 2019.",
+    your_img_url: "https://images.pexels.com/photos/8566531/pexels-photo-8566531.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 };
 
 const dataabout = {
@@ -102,7 +102,7 @@ const dataportfolio = [{
     {
         img: "https://www.upwork.com/att/download/portfolio/persons/uid/817399807765053440/profile/projects/files/1304890762304069632",
         description: "This is a really awesome and great single page react application that allows the people to add meetings by theme-self. The people can check-in at a different meeting also. The admin user of the meeting can delete other users, can send theme emails, or give them star marks. The user can search and filter to find out other people or can select them randomly.",
-        link: "https://www.upwork.com/freelancers/~01a5b413bec1d6bb8a?p=1304887695148601344",
+        link: "https://upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com/profile/portfolio/thumbnail/8a7d8ffacdef1cd354fef7bdf2898a60?response-content-disposition=inline;+filename=%22image_original%22;+filename*=utf-8%27%27image_original&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPT//////////wEaCXVzLXdlc3QtMiJHMEUCICzhi8TzUJvWH8TBnsTzL0XsQbx4IvzS4F93JdDVXxHcAiEAo/SZhja5JHCu2CSSqaeegqEfWvF%2BgS3LE5GQrUdQPYYqkAUIXRAAGgw3Mzk5MzkxNzM4MTkiDJDM010Hlzpw/635KyrtBIoPJtZG1eC0xWGcc%2Bf0lai5qAuNk82mLs7rujS7L8lohTSD2M8nLCTbYnsg6ID1NYpg/vV1fZXR6Ei%2B80PknAgpVRp3nytm6VHD9GhSQzYyfB02sQ40XG4/4M%2Bz4purLzt1%2BpA3npF6jF/3vP4WUQQykByUTm4%2BuTLTxwEaR6oz76XGDUYGqmVE7BToHVTE8JTLxG1COhaCo/eJj13T18yoyPE/huYaxKqVknh86Un2CBeCHEvAWWP8SkWSkVNvVez5cMxfQMTa5xMtivC4RgMZSTkzqoFTOyp5g8g0NX%2Bi4VXco6sQac5rLiGBgVygSsAiHf1l62wJ4pfpBgrpVuCh/SjvJG4LFtFgFfMajW7KODXMQcWK%2BjJRKmGlNQcHAplqgfpyApBxQ%2BIBTsMhEnoNEu4g1xK2QRj33T5OzjbiayxMCm6dLhSr3AukfNYBlrihICe9tOiARBvJ1qdC1zzEjFukNNeUiZJB1Kig/ZaOVtQCsOUiOF3WtqyegiLznFAAc2vzu0SDdF6Xw6xVHAvyDmwqZzA%2BizzmVoY1X9v3efvBjF3lzqJQH0V6VCKiih0yow6LHMriCYKma66P%2BgwyuMb%2BsTPXRiDzyUoX%2Bdm184sNN3jz/Ovucl5Z2dXg79DXDlzIKi7NzBZIVRt0W4Ti90K1B3XTkUlLW3JyF8jzn8XzBg%2BEErFw7hML0gRFp58Y6neyLPA1JBFQIBW35xtqCiKnlCVW5m0D4QB8hPc8H4PWw4Esnbx3lL6gSWD/NmMGXJ3XgxLvuA40imPoWw%2BKm5/2fIoQmD1wdjOOjojufJuNaLclTaYV5W/HOjC6jpq/BjqbAWGouHM9kZvgEtXi25CVdc00jmlZownpqd8oH5MPRI3djVX%2B17/SCuSyJpKvkR5jwnxRrZkt9dww9O6gDyPuzZgKNRMuwm2t8cd334O9ZVTuDK4U85OhrNHaIhdQZFeDhFrzyrBqpbDgyNSYzSBTOKkNZDyRzHQEN6ef1DGqLgq9LRnYr9x5JkZ2sGhBZCudp5lCo9g/YdAQSMpC&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250328T114617Z&X-Amz-SignedHeaders=host&X-Amz-Expires=899&X-Amz-Credential=ASIA2YR6PYW5SPKXKCCH/20250328/us-west-2/s3/aws4_request&X-Amz-Signature=dc2a956692a5ed51067b8b849fbeb38390c91e0bd974a38708972dc4071fbdea",
     },
     {
         img: "https://upwork-usw2-prod-agora-file-storage.s3.us-west-2.amazonaws.com/profile/portfolio/thumbnail/942d7b8ebe0d009152ec31747e9e890d?response-content-disposition=inline;+filename=%22image_original%22;+filename*=utf-8%27%27image_original&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPD//////////wEaCXVzLXdlc3QtMiJGMEQCIDpqfD/Zz6jCm3FGVV2bHMT04jaFWV9wVgY%2BWu24RyJ8AiA8GdbMuqvXxWpp/Dfryu3J8XH3MtzNKiOHFawGIHaAkCqQBQhZEAAaDDczOTkzOTE3MzgxOSIMv7PaUZEQT6jhQRgYKu0Edr351pv/YGeLyvXhXbz8lr6RNHfN4QpfrZxI32e9bpNesSc886ucoJkTC6RZRihk9phdNQF/6oA/chvy0GZgt4KNcz8LvtLcrZ/riX6b4ALBiCB5fdK9kkYKbPjJoPkKv1TRy7YAG9itgNOH8C4Ik4z9QUQohown9sdiaNIbdhqZLKz1xxUU0QZNBi37EYm3sOL1EiS00pnim%2BJ/pZPXIJfsqX0BMpMCM5aXR8TfvSPewqwkH%2BA7BTeUO1RSfTq9c2ntP/CSOfJCgaWqySKfhKu%2BxoPrY6Oe2YfeO1C8H4TkcwruN8ZoM1wjPg5ct98KgGsaIYN2Onerb3Mxifei6ouj5bGlTi34%2B8tmxob7ckWacVr0hKdqr7KSM9MyCFx6JXMlTCh/%2B9saA727TwL3yHSfsYfr5gin4kDmq5il5l0R%2B5UJ6vYiXNSbWjKA%2BI3UN7R5wxY1ECQMCcAn0wv5z2CXAcCVUE3ZPI80DOLtAcga%2BHI642GzRb%2Bdyw0XpH1qymF9C6YAk5Mj5O%2Bkd/7vsS67d7B1WhLvjTU5vKNRsmo4RZeoBzAItdQg5lwDckWwQE6%2BgFlXLjWeE9iUWdd6hk/2bxtzUgs7CK7ybvHauFBD4D3GpH%2Bxd7rleRodRQMyUXeT9j7XI6Wj17nKWjD6sl%2BZLvB6/O/8hj40kx/sF9Y9PJntRLeRWBPjYXV6JQ6VejxaWgwUBICWrapBCs6lbO/ICn8kFnpoaVkqlc/YMbX/szq42qWL5K/0RBuvq55WNVCG%2B2yeBazlN4GFCaW8%2Bed4e1T9gYiYxVAEJzOqik8/sXXSR11JN6B2OqHoMKqnmb8GOpwB1QpztdZmOMFqXWsn6WsPv/Pc6nI8UwwtHVmdhIDoZxbjrl7Zmpz9nen61mv8yIxbVWknoxnc7yQWTy2DmN2Y4it6ZrSrH3KKWSPKeaKk6xOfuM9hWOnaX1yHKdqiZj2iBZ9NeDNwljKTUXWK24kd%2BPopnqPEpNZgKIhZsr0dOs5qpNqZMRTEqwAgXdCRZZrYXP3wnRp/iamgzfcQ&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250328T081824Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=ASIA2YR6PYW5QQSV5PZY/20250328/us-west-2/s3/aws4_request&X-Amz-Signature=31a98eb29f5504f52c87c14f216df7844ffa3e2b34e4d3af158f475340d71115",
